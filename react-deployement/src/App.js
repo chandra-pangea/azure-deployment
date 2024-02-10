de9a1456e9b8eb48a3f73ae503f4916ff6485352
chandra prakash <cp150496@gmail.com>
2024-02-10 23:00:55 +0530

    mssaged added

--- react-deployement/src/App.js
+++ react-deployement/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+    <div className="main">
+      <p className="heading">I LOVE YOU MY MUTKI BUBU</p>
+      <p className="heading">TUMHARA PATLU</p>
+
+    </div>
+    <div className="main">
+      <div class="heart-shape"></div>
+    </div>
+    <div className="main">
+      <p className="paragraph"> 
+      Happy Teddy Day, sweetheart. Sending you a cute heart as a symbol of love. Love you, baby. Be my teddy bear, always.
+      </p>
+      <p className="paragraph">On this special day, let this heart bear express my feelings that words fail to convey. You're my comfort, my joy, and my love. Happy Teddy Day, my darling!</p>
+    </div>
     </div>
   );
 }
